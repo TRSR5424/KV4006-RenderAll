be1401f5a16b97296dd7fa344a4868ed4cc174a4
jjsanderson <jonathan@quernstone.com>
2024-12-06 11:01:49 +0000

    More path fixes for Windows. Maybe.

--- render_all.py
+++ render_all.py
@@ -9,9 +9,12 @@ if __name__ == "__main__":
 
     # The *args bit here avoids some nasty errors when the watch triggers
     # on a directory create/delete event.
-    server.watch("templates/**/*.j2", lambda path, *args: render_all(path))
-    server.watch("templates/**/*.jinja", lambda path, *args: render_all(path))
-    server.watch("templates/**/*.html", lambda path, *args: render_all(path))
-    server.watch("data/*.json", lambda path, *args: render_all(path))
-    server.watch("data/*.[yY][aA][mM][lL]", lambda path, *args: render_all(path))
+    # The isinstance(path, list) parts check if the path is a list of paths,
+    # which can happen when a directory is created or deleted. If so, they pass
+    # only the first path to render_all, again to avoid an error.
+    server.watch("templates/**/*.j2", lambda path, *args: render_all(path[0] if isinstance(path, list) else path))
+    server.watch("templates/**/*.jinja", lambda path, *args: render_all(path[0] if isinstance(path, list) else path))
+    server.watch("templates/**/*.html", lambda path, *args: render_all(path[0] if isinstance(path, list) else path))
+    server.watch("data/*.json", lambda path, *args: render_all(path[0] if isinstance(path, list) else path))
+    server.watch("data/*.[yY][aA][mM][lL]", lambda path, *args: render_all(path[0] if isinstance(path, list) else path))
     server.serve(root="site")
